533021ba9e899acf0b516067f6d203673e16ffa3
kali <nmqvl@kali.kali>
2025-06-15 07:57:16 -0300

    feat(View): adciona evento de adcionar e remover infos da entidade

--- src/main/java/br/com/poo/view/Visor.java
+++ src/main/java/br/com/poo/view/Visor.java
@@ -12,7 +12,8 @@ public class Visor extends JPanel {
     private static final int BASE_WIDTH = 700;
     private static final int BASE_HEIGHT = 400;
 
-    private final List<JComponent> componentes = new ArrayList<>();
+    private final List<JComponent> componentesFixos = new ArrayList<>();
+    private final List<JComponent> componentesInfo = new ArrayList<>();
     private JTextField[] camposDigito = new JTextField[2];
 
     public Visor() {
@@ -21,7 +22,7 @@ public class Visor extends JPanel {
         setBorder(BorderFactory.createLineBorder(Color.BLACK, 10, true));
         setPreferredSize(new Dimension(BASE_WIDTH, BASE_HEIGHT));
 
-        inicializarComponentes();
+        iniciarTelaSimples();
 
         addComponentListener(new ComponentAdapter() {
             @Override
@@ -31,48 +32,27 @@ public class Visor extends JPanel {
         });
     }
 
-    private void inicializarComponentes() {
-        adicionarLabel("SEU VOTO PARA:", 17, new Rectangle(20, 10, 300, 20));
-        adicionarLabel("Governador", 22, new Rectangle(300, 45, 300, 30));
-        adicionarLabel("Número:", 16, new Rectangle(20, 100, 100, 20));
-        adicionarLabel("Nome:", 16, new Rectangle(20, 190, 100, 20));
-        adicionarLabel("Vôlei", 16, new Rectangle(100, 190, 200, 20));
-        adicionarLabel("Partido:", 16, new Rectangle(20, 220, 100, 20));
-        adicionarLabel("PEsp", 16, new Rectangle(100, 220, 100, 20));
-        adicionarLabel("Vice-Governador:", 16, new Rectangle(20, 250, 160, 20));
-        adicionarLabel("Tênis", 16, new Rectangle(170, 250, 200, 20));
-        adicionarLabel("Governador", 12, new Rectangle(585, 190, 100, 20));
-        adicionarLabel("Vice-Governador", 12, new Rectangle(568, 360, 120, 20));
-
-        adicionarLabel(
-            "<html><div style='letter-spacing: 1.5px;'><hr style='border: 1px solid black;'><br><br>"
-            + "<div>Aperte a tecla:</div>"
-            + "<div><b>CONFIRMA</b> para CONFIRMAR este voto</div>"
-            + "<div><b>CORRIGE</b> para REINICIAR este voto</div></div></html>",
-            15, new Rectangle(20, 285, 500, 100)
-        );
-
-        adicionarFoto(new Rectangle(570, 85, 100, 100));
-        adicionarFoto(new Rectangle(570, 260, 100, 100));
-
+    private void iniciarTelaSimples() {
+        iniciarHeader();
         adicionarCamposNumericos(2, new Rectangle(110, 85, 120, 60));
     }
 
-    private void adicionarLabel(String texto, int tamanhoFonte, Rectangle bounds) {
+    private void iniciarHeader() {
+        adicionarLabel("SEU VOTO PARA:", 17, new Rectangle(20, 10, 300, 20), false);
+        adicionarLabel("Governador", 22, new Rectangle(300, 45, 300, 30), false);
+    }
+
+    private void adicionarLabel(String texto, int tamanhoFonte, Rectangle bounds, boolean ehInfo) {
         JLabel label = new JLabel(texto);
         label.setFont(new Font("Arial", Font.PLAIN, tamanhoFonte));
         label.setBounds(bounds);
-        componentes.add(label);
-        add(label);
+        adicionarComponente(label, ehInfo);
     }
 
-    private void adicionarFoto(Rectangle bounds) {
-        JPanel foto = new JPanel();
-        foto.setBackground(new Color(230, 230, 230));
-        foto.setBorder(new LineBorder(Color.BLACK, 1));
-        foto.setBounds(bounds);
-        componentes.add(foto);
-        add(foto);
+    private void adicionarComponente(JComponent comp, boolean ehInfo) {
+        if (ehInfo) componentesInfo.add(comp);
+        else componentesFixos.add(comp);
+        add(comp);
     }
 
     private void adicionarCamposNumericos(int quantidade, Rectangle bounds) {
@@ -89,28 +69,7 @@ public class Visor extends JPanel {
             painelNumeros.add(camposDigito[i]);
         }
 
-        componentes.add(painelNumeros);
-        add(painelNumeros);
-    }
-
-    public void onAcao(String acao) {
-        if (acao.matches("\\d+")) inserirDigito(acao);
-        else acionarBotao(acao);
-    }
-
-    private void inserirDigito(String acao) {
-        if (camposDigito[0].getText().isEmpty()) camposDigito[0].setText(acao);
-        else if (camposDigito[1].getText().isEmpty()) camposDigito[1].setText(acao);
-    }
-
-    private void acionarBotao(String acao) {
-        if (acao.equals("CORRIGE")) apagarTextoCampos();
-        if (acao.equals("CONFIRMA")) System.out.println("voto confirmado");
-        if (acao.equals("BRANCO")) System.out.println("voto em branco");
-    }
-
-    private void apagarTextoCampos() {
-        for (JTextField campo : camposDigito) campo.setText("");
+        adicionarComponente(painelNumeros, false);
     }
 
     private JTextField criarCampoNumerico(int largura, int altura, int x, int y) {
@@ -135,23 +94,105 @@ public class Visor extends JPanel {
         });
     }
 
+    public void onAcao(String acao) {
+        if (acao.matches("\\d+")) inserirDigito(acao);
+        else acionarBotao(acao);
+    }
+
+    private void inserirDigito(String acao) {
+        if (camposDigito[0].getText().isEmpty()) camposDigito[0].setText(acao);
+        else if (camposDigito[1].getText().isEmpty()) {
+            camposDigito[1].setText(acao);
+            adicionarInfosEntidade();
+        }
+    }
+
+    private void acionarBotao(String acao) {
+        if (acao.equals("CORRIGE")) apagarTextoCampos();
+        if (acao.equals("CONFIRMA")) System.out.println("voto confirmado");
+        if (acao.equals("BRANCO")) System.out.println("voto em branco");
+    }
+
+    private void apagarTextoCampos() {
+        for (JTextField campo : camposDigito) campo.setText("");
+        removerInfosEntidade();
+    }
+
+    private void removerInfosEntidade() {
+        for (JComponent comp : componentesInfo) {
+            remove(comp);
+        }
+        componentesInfo.clear();
+
+        revalidate();
+        repaint();
+    }
+
+    private void adicionarInfosEntidade() {
+        adicionarCamposInfo();
+        adicionarCamposFoto();
+        adicionarCampoAjuda();
+        revalidate();
+        repaint();
+    }
+
+    private void adicionarCamposInfo() {
+        adicionarLabel("Número:", 16, new Rectangle(20, 100, 100, 20), true);
+        adicionarLabel("Nome:", 16, new Rectangle(20, 190, 100, 20), true);
+        adicionarLabel("Vôlei", 16, new Rectangle(100, 190, 200, 20), true);
+        adicionarLabel("Partido:", 16, new Rectangle(20, 220, 100, 20), true);
+        adicionarLabel("PEsp", 16, new Rectangle(100, 220, 100, 20), true);
+        adicionarLabel("Vice-Governador:", 16, new Rectangle(20, 250, 160, 20), true);
+        adicionarLabel("Tênis", 16, new Rectangle(170, 250, 200, 20), true);
+        adicionarLabel("Governador", 12, new Rectangle(585, 190, 100, 20), true);
+        adicionarLabel("Vice-Governador", 12, new Rectangle(568, 360, 120, 20), true);
+    }
+
+    private void adicionarCamposFoto() {
+        adicionarFoto(new Rectangle(570, 85, 100, 100));
+        adicionarFoto(new Rectangle(570, 260, 100, 100));
+    }
+
+    private void adicionarFoto(Rectangle bounds) {
+        JPanel foto = new JPanel();
+        foto.setBackground(new Color(230, 230, 230));
+        foto.setBorder(new LineBorder(Color.BLACK, 1));
+        foto.setBounds(bounds);
+        adicionarComponente(foto, true);
+    }
+
+    private void adicionarCampoAjuda() {
+        adicionarLabel(
+            "<html><div style='letter-spacing: 1.5px;'><hr style='border: 1px solid black;'><br><br>"
+            + "<div>Aperte a tecla:</div>"
+            + "<div><b>CONFIRMA</b> para CONFIRMAR este voto</div>"
+            + "<div><b>CORRIGE</b> para REINICIAR este voto</div></div></html>",
+            15, new Rectangle(20, 285, 500, 100), true
+        );
+    }
+
     private void redimensionarComponentes() {
         double escalaX = getWidth() / (double) BASE_WIDTH;
         double escalaY = getHeight() / (double) BASE_HEIGHT;
 
-        for (JComponent comp : componentes) {
-            Rectangle original = comp.getBounds();
-            int x = (int) (original.x * escalaX);
-            int y = (int) (original.y * escalaY);
-            int largura = (int) (original.width * escalaX);
-            int altura = (int) (original.height * escalaY);
-            comp.setBounds(x, y, largura, altura);
-
-            Font fonteAntiga = comp.getFont();
-            float novoTamanho = (float) (fonteAntiga.getSize2D() * Math.min(escalaX, escalaY));
-            comp.setFont(fonteAntiga.deriveFont(novoTamanho));
-        }
+        for (JComponent comp : componentesFixos) redimensionar(comp, escalaX, escalaY);
+        for (JComponent comp : componentesInfo) redimensionar(comp, escalaX, escalaY);
 
         repaint();
+    }
+
+    private void redimensionar(JComponent comp, double escalaX, double escalaY) {
+        Rectangle original = comp.getBounds();
+        int x = (int) (original.x * escalaX);
+        int y = (int) (original.y * escalaY);
+        int largura = (int) (original.width * escalaX);
+        int altura = (int) (original.height * escalaY);
+        comp.setBounds(x, y, largura, altura);
+
+        Font fonte = comp.getFont();
+        if (fonte != null) {
+            float novoTamanho = (float) (fonte.getSize2D() * Math.min(escalaX, escalaY));
+            comp.setFont(fonte.deriveFont(novoTamanho));
+        }
     }
 }
